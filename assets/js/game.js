6dd264546789e1aafd9412a59b11bb58fef255cc
Cayean <collinayala@gmail.com>
2022-07-28 22:23:43 -0500

    add fight call to function

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+window.alert("This is an alert! Javascript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+
+//this creates a function called "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// Call to fight function
+
+fight();
