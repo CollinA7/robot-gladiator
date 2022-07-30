cb4a57d79e1e7bb746145017834bcb032b81c629
Cayean <collinayala@gmail.com>
2022-07-29 21:21:04 -0500

    Initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,18 +8,28 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+var playerMoney = 10;
+
+// Fight function start
 
 var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+    
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
-// Player attack function 
-enemyHealth = enemyHealth - playerAttack;
 
-console.log(
+    //If the player chooses to fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+    
+    // Player attack function 
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
+    //enemy health check
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
     }
@@ -29,19 +39,43 @@ console.log(
     }
 
     // Enemy attack function
-
   playerHealth = playerHealth - enemyAttack;
 
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
+    //Player Health check
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
     }
     else{
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+
+    // If player chooses to skip
+} else if (promptFight === "skip" || promptFight ==="SKIP") {
+    
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+    window.alert(playerName + " has chosen to skip the fight!");
+    
+    playerMoney = playerMoney - 2;
+    }
+    
+    //if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
+    
+    
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
+
 };
 
 fight();
