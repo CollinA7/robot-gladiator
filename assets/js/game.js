acb5f848d1636ed97f045e39b3022412b86fdd65
Cayean <collinayala@gmail.com>
2022-07-30 15:14:53 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,13 +77,29 @@ var fight = function(enemyName) {
             }
             };
             
-for(var i = 0; i < enemyNames.length; i++) {
+    for(var i = 0; i < enemyNames.length; i++) {
 
-    var pickedEnemyName = enemyNames[i];
+    if (playerHealth > 0) {
+        
+        //Let player know what round they are in, remmeber that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+        
+        //pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        
+        //Resets enemyHealth before starting new fight
+        enemyHealth = 50;
+        
+        debugger;
+        
+        fight(pickedEnemyName);
+    }
 
-    enemyHealth = 50;
-
-    fight(pickedEnemyName);
+    else {
+        window.alert("You have lost your robot in battle! Game Over!")
+        
+        break;
+    }
 }
 
 //fight();
