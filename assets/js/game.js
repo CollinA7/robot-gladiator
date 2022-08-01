8d1422cddc690f0baaeced1eae1f9141c9bdd115
Cayean <collinayala@gmail.com>
2022-07-31 20:26:44 -0500

    add fightOrSkip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,35 @@
 
 // Fight function start
+var fightOrSkip = function() {
+    // ask player if they'd like to fight or skip ising FightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+
+
+    if (promptFight === "" || promptFight === null) {
+        
+        window.alert("You need to provide a valid answer! Please try again!");
+
+        return fightOrSkip();
+    }
+
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you's like to quit?");
+
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+
+            //subtract money from playerMoney for skipping
+            playerInfo.playerMoney = playerInfo.money - 10;
+            shop();
+        }
+    }
+}
 
 var fight = function(enemy) {
     
